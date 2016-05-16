b61be155b602aee88d543d258d405ec0de2475d7
Elizabeth Linn <elise_81@yahoo.com>
2016-05-15 19:49:43 -0700

    object literals

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-// Require
-var greet = require('./greet.js');
-greet.sayHello();
+var person = {
+    firstname: 'Elise',
+    lastname: 'Linn',
+    greet: function () {
+        console.log('Hello, ' + this.firstname + ' ' + this.lastname + '.');
+    }
+};
+
+person.greet();
+console.log('What\'s that person\'s first name again? ' + person['firstname']);
