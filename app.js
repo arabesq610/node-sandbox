c5bc3fb2f142a850300ae9e891dbee2c2aab95ac
Elizabeth Linn <elise_81@yahoo.com>
2016-05-15 21:19:30 -0700

    module patterns v4 illustrating the revealing module pattern

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 var greet1 = require('./greet1');
 greet1();
 
+///////////////////////////////////////////////////////////////////////////////
+
 var greet2 = require('./greet2').greet;
 greet2();
+
+///////////////////////////////////////////////////////////////////////////////
 
 var greet3 = require('./greet3');
 greet3.greet();
 
 greet3.greeting = 'New and improved greeting';
 
-var greet3b = require('./greet3'); // watch out! this one is just pointing to the same location in memory as greet3 because it caches the module
+///////////////////////////////////////////////////////////////////////////////
 
-greet3b.greet(); // will reflect the changed value of greet3.greeting
+// watch out! this one is just pointing to the same location in memory as greet3 because it caches the module
+var greet3b = require('./greet3');
+// will reflect the changed value of greet3.greeting
+greet3b.greet();
+
+///////////////////////////////////////////////////////////////////////////////
 
 var greet4 = require('./greet4');
 var greeter = new greet4();
 greeter.greet();
+
+///////////////////////////////////////////////////////////////////////////////
+
+// reveaing module pattern
+var greet5 = require('./greet5').greet;
+greet5();
+
+///////////////////////////////////////////////////////////////////////////////
---
+++ greet5.js
@@ -0,0 +1,10 @@
+var greeting = 'Hello world!!!!!';
+
+function greet() {
+    'use strict';
+    console.log(greeting);
+}
+
+module.exports = {
+    greet: greet
+};
