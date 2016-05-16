44aeab1d02b6b60b6c141dd6815d43d99499e5b5
Elizabeth Linn <elise_81@yahoo.com>
2016-05-15 20:16:58 -0700

    iifes

--- app.js
+++ app.js
@@ -1,37 +1,8 @@
-var a, c;
-
-// this function wants a primitive value
-function changePrimitive(b) {
+(function () {
     'use strict';
-    b = 2;
-}
+    var firstname = 'IIFE Elise';
+    console.log(firstname);
+}());
 
-a = 1;
-
-// we feed the function a primitive value
-changePrimitive(a);
-
-// still 1! passed by value
-console.log(a);
-
-
-// now time for some pass by reference...
-
-// this function wants a complex value
-function changeComplex(d) {
-    'use strict';
-    d.prop1 = function () {};
-    d.prop2 = {};
-}
-
-// c is an empty object
-c = {};
-
-// c.prop1 is an empty object
-c.prop1 = {};
-
-// now pass by reference to the hungry function
-changeComplex(c);
-
-// lo and behold, it is now a function because it was passed by reference
-console.log(c);
+var firstname = 'Global Elise';
+console.log(firstname);
