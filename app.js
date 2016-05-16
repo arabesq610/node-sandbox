faeaadee214027d05006fe8ca5e910114af9c944
Elizabeth Linn <elise_81@yahoo.com>
2016-05-15 20:54:31 -0700

    module patterns v2 illustrating module caching

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-var greet = require('./greet1');
+var greet1 = require('./greet1');
+greet1();
+
 var greet2 = require('./greet2').greet;
-greet();
 greet2();
+
+var greet3 = require('./greet3');
+greet3.greet();
+
+greet3.greeting = 'New and improved greeting';
+
+var greet3b = require('./greet3'); // watch out! this one is just pointing to the same location in memory as greet3 because it caches the module
+
+greet3b.greet(); // will reflect the changed value of greet3.greeting
--- greet2.js
+++ greet2.js
@@ -1,3 +1,4 @@
 module.exports.greet = function () {
-    console.log('Hello world');
-}
+    'use strict';
+    console.log('Hello world!!');
+};
